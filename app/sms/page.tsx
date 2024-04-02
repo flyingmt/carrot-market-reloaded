7cee6770c2220ba88ef47dfab909b57db305886a
flyingmt <flyingmt@gmail.com>
2024-04-03 08:49:21 +0900

    #7.6 완료

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,10 +3,15 @@
 import Button from '@/components/button';
 import Input from '@/components/input';
 import { useFormState } from 'react-dom';
-import { smsVerification } from './actions';
+import { smsLogin } from './actions';
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -15,14 +20,26 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? 'Verify Token' : 'Send Verication SMS'} />
       </form>
     </div>
   );
