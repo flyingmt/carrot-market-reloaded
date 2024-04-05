45e467f3cd03fc61aa57a2232de1f996e53b1c07
flyingmt <flyingmt@gmail.com>
2024-04-05 09:11:51 +0900

    #8.6 완료

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,10 +1,12 @@
 "use server";
-import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from '@/lib/constatns';
-import {z} from 'zod';
 
-function checkUsername(username:string) {
-    return !username.includes("potato");
-}
+import bcrypt from 'bcrypt';
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from '@/lib/constatns';
+import db from '@/lib/db';
+import {z} from 'zod';
+import { redirect } from 'next/navigation';
+import getSession from '@/lib/session';
+
 
 function checkPasswords({password, confirm_password}: {password: string, confirm_password: string}) {
     return password === confirm_password;
@@ -17,12 +19,47 @@ const formSchema = z.object({
             required_error: "Username is required!",
         }
     )
-    .toLowerCase()
-    .trim()
-    .refine(checkUsername, "Username cannot contain the word 'potato'!"),
+        .toLowerCase()
+        .trim(),
     email: z.string().email().toLowerCase(),
     password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, "Password is too weak!"),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+}).superRefine(async ({username}, ctx) => {
+    const user = await db.user.findUnique({
+        where: {
+            username,
+        },
+        select: {
+            id: true,
+        },
+    });
+    if (user) {
+        ctx.addIssue({
+            code: 'custom',
+            message: "This username is alread taken",
+            path: ["username"],
+            fatal: true,
+        });
+        return z.NEVER;
+    }
+}).superRefine(async ({email}, ctx) => {
+    const user = await db.user.findUnique({
+        where: {
+            email,
+        },
+        select: {
+            id: true,
+        },
+    });
+    if (user) {
+        ctx.addIssue({
+            code: 'custom',
+            message: "This email is alread taken",
+            path: ["email"],
+            fatal: true,
+        });
+        return z.NEVER;
+    }
 }).refine(checkPasswords, {
     message: "Passwords do not match!",
     path: ["confirm_password"],
@@ -36,10 +73,27 @@ export async function createAccount(prevState: any, formData:FormData) {
         confirm_password: formData.get('confirm_password'),
     };
 
-    const result = formSchema.safeParse(data);
+    const result = await formSchema.safeParseAsync(data);
     if (!result.success) {
         return result.error.flatten();
     } else {
-        console.log(result.data);
+        const hashedPassword = await bcrypt.hash(result.data.password, 12);
+        const user = await db.user.create({
+            data: {
+                username: result.data.username,
+                email: result.data.email,
+                password: hashedPassword,
+            },
+            select: {
+                id: true,
+            },
+        });
+        
+        const session = await getSession();
+       
+        session.id = user.id;
+        await session.save();
+
+        redirect("/profile");
     }
 }
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,10 +1,28 @@
 "use server";
 
+import bcrypt from 'bcrypt';
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from '@/lib/constatns';
+import db from '@/lib/db';
 import {z} from 'zod';
+import getSession from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where : {
+        email,
+    },
+    select : {
+        id: true,
+    },
+  });
+
+  return Boolean(user);
+}
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z.string().email().toLowerCase()
+    .refine(checkEmailExists, "Email does not exist!"),
   password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, "Password is too weak!"),
 });
 
@@ -14,10 +32,37 @@ export async function login(prevState: any, formData: FormData) {
     password: formData.get('password'),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // if the user is found, check password hash
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email
+      },
+      select: {
+        id: true,
+        password: true
+      }
+    });
+
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? "xxx");
+    
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["Invalid password!"],
+          email: [],
+        }        
+      };
+    }
+
+    // redirect "/profile"
   }
 }
