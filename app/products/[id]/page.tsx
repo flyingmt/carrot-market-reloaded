0021af15af97b17aaeb6e5952ca374483bb8ad8c
flyingmt <flyingmt@gmail.com>
2024-04-19 08:19:47 +0900

    #10.8 완료

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -50,10 +50,10 @@ export default async function ProductDetail(
     return (
         <div>
             <div className="relative aspect-square">
-                <Image fill src={product.photo} alt={product.title} />
+                <Image fill src={product.photo} className="object-cover" alt={product.title} />
             </div>
             <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
-                <div className="size-10 rounded-full">
+                <div className="size-10 overflow-hidden rounded-full">
                     { product.user.avatar !== null ?
                         <Image src={product.user.avatar}
                                width={40} height={40} alt={product.user.username} />
